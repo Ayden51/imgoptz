1153816eae618a4c78e4a1a833390d6d7f74b275
Ayden51 <dconghuy0@gmail.com>
2026-07-11 19:05:11 +0700

    feat: update run script to execute build and launch imgoptz executable

--- run.sh
+++ run.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 
-odin run src -out:dist/imgoptz.exe -target:windows_amd64 -subsystem:console -o:speed -debug -strict-style -vet -vet-packages:main -vet-unused-procedures -vet-tabs -disallow-do -warnings-as-errors
+./build.sh
+./dist/imgoptz.exe
